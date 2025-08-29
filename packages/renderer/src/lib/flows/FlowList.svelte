becf39e3d17640532cf3f0385ad9139c27aa47a6
Simon Rey <51708585+eqqe@users.noreply.github.com>
2025-08-29 11:04:59 +0200

    feat: add button to restart goose if extension installed  (#106)
    * feat: add button to restart goose if extension installed
    
    Signed-off-by: Simon Rey <sfbrey+eqqe@gmail.com>
    
    feat: add button to restart goose if extension installed
    
    Signed-off-by: Simon Rey <sfbrey+eqqe@gmail.com>
    
    feat: add button to restart goose if extension installed
    
    Signed-off-by: Simon Rey <sfbrey+eqqe@gmail.com>
    
    feat: add button to restart goose if extension installed
    
    Signed-off-by: Simon Rey <sfbrey+eqqe@gmail.com>
    
    feat: add button to restart goose if extension installed
    
    Signed-off-by: Simon Rey <sfbrey+eqqe@gmail.com>
    
    Revert "feat: add button to restart goose if extension installed"
    
    This reverts commit fb4c319bcef67a88ee86b0e68fecb48217084f35.
    
    feat: add button to restart goose if extension installed
    
    Signed-off-by: Simon Rey <sfbrey+eqqe@gmail.com>
    
    * feat: add button to restart goose if extension installed
    
    Signed-off-by: Simon Rey <sfbrey+eqqe@gmail.com>
    
    ---------
    
    Signed-off-by: Simon Rey <sfbrey+eqqe@gmail.com>

--- packages/renderer/src/lib/flows/FlowList.svelte
+++ packages/renderer/src/lib/flows/FlowList.svelte
@@ -33,7 +33,11 @@ function navigateToCreateFlow(): void {
   });
 }
 
-let hasInstalledFlowProviders = $derived(window.hasInstalledFlowProviders());
+let hasInstalledFlowProviders = $state(window.hasInstalledFlowProviders());
+
+function retryCheck(): void {
+  hasInstalledFlowProviders = window.hasInstalledFlowProviders();
+}
 </script>
 
 <NavPage searchEnabled={false} title="Flows">
@@ -63,7 +67,7 @@ let hasInstalledFlowProviders = $derived(window.hasInstalledFlowProviders());
             key={key}
           />
         {:else}
-          <NoFlowProviders />
+          <NoFlowProviders {retryCheck} />
         {/if}
       {/await}
     </div>
