9a4d7e60b5d41dc56264fc80ed61940f6f247b77
Simon Rey <51708585+eqqe@users.noreply.github.com>
2025-09-02 16:54:39 +0200

    fix(chat): when MCP not installed invite user with link in toast (#230)
    * fix(chat): when MCP not installed,
    
    Signed-off-by: Simon Rey <sfbrey+eqqe@gmail.com>
    
    * refactor: use function instead of const
    
    Signed-off-by: Simon Rey <sfbrey+eqqe@gmail.com>
    
    * refactor: add type
    
    Signed-off-by: Simon Rey <sfbrey+eqqe@gmail.com>
    
    * refactor: add interface
    
    Signed-off-by: Simon Rey <sfbrey+eqqe@gmail.com>
    
    ---------
    
    Signed-off-by: Simon Rey <sfbrey+eqqe@gmail.com>

---
+++ packages/renderer/src/lib/chat/components/McpsToInstallToast.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
+import type { components } from 'mcp-registry';
+import Fa from 'svelte-fa';
+
+import { handleNavigation } from '/@/navigation';
+import { NavigationPage } from '/@api/navigation-page';
+
+interface Props {
+  mcpsToInstall: readonly components['schemas']['ServerDetail'][];
+}
+let { mcpsToInstall }: Props = $props();
+</script>
+
+<div>
+  <p class="text-sm font-medium">The following MCPs are required to use this suggestion:</p>
+  <ul class="pt-2">
+    {#each mcpsToInstall as mcp (mcp.id)}
+      <li>
+        <button
+          class="w-full text-left p-2 rounded-md hover:bg-charcoal-600"
+          onclick={(): void =>
+            handleNavigation({
+              page: NavigationPage.MCP_INSTALL_FROM_REGISTRY,
+              parameters: { serverId: mcp.id ?? '' },
+            })}>
+            <div class='flex items-center'>
+          <Fa icon={faDownload} class="mr-2" />
+          {mcp.name}</div>
+        </button>
+      </li>
+    {/each}
+  </ul>
+</div>
--- packages/renderer/src/lib/chat/components/suggested-actions.svelte
+++ packages/renderer/src/lib/chat/components/suggested-actions.svelte
@@ -3,9 +3,11 @@ import type { Chat } from '@ai-sdk/svelte';
 import { fly } from 'svelte/transition';
 import { toast } from 'svelte-sonner';
 
+import { mcpRegistriesServerInfos } from '/@/stores/mcp-registry-servers';
 import { mcpRemoteServerInfos } from '/@/stores/mcp-remote-servers';
 import type { MCPRemoteServerInfo } from '/@api/mcp/mcp-server-info';
 
+import McpsToInstallToast from './McpsToInstallToast.svelte';
 import { Button } from './ui/button';
 
 let {
@@ -18,12 +20,19 @@ let {
   mcpSelectorOpen: boolean;
 } = $props();
 
-const suggestedActions = [
+type SuggestedAction = {
+  title: string;
+  label: string;
+  action: string;
+  requiredMcp?: string[];
+};
+
+const suggestedActions: SuggestedAction[] = [
   {
     title: 'What are the last 5 issues of Github',
     label: 'repository podman-desktop/podman-desktop?',
     action: 'What are the last 5 issues of Github repository podman-desktop/podman-desktop?',
-    requiredMcp: ['internal:123e4567-e89b-12d3-a456-426614172000:0'],
+    requiredMcp: ['123e4567-e89b-12d3-a456-426614172000'],
   },
   {
     title: 'Write code to',
@@ -41,6 +50,60 @@ const suggestedActions = [
     action: 'What is the weather like in San Francisco?',
   },
 ];
+
+async function onclick(suggestedAction: SuggestedAction): Promise<void> {
+  const mcpsToInstall = suggestedAction.requiredMcp?.flatMap(id => {
+    const mcpInstalledInfo = $mcpRemoteServerInfos.find(mcp => mcp.infos.serverId === id);
+
+    if (mcpInstalledInfo) {
+      return [];
+    }
+
+    const mcpInfo = $mcpRegistriesServerInfos.find(mcp => mcp.id === id);
+
+    if (!mcpInfo) {
+      throw Error(`Suggested action ${suggestedAction.action} requires MCP with id ${id} but it was not found.`);
+    }
+
+    return [mcpInfo];
+  });
+
+  if (mcpsToInstall?.length) {
+    toast.error(McpsToInstallToast, {
+      componentProps: {
+        mcpsToInstall,
+      },
+    });
+    return;
+  }
+
+  const mcpsToSelect = suggestedAction.requiredMcp?.flatMap(id => {
+    const selected = selectedMCP.find(mcp => mcp.infos.serverId === id);
+
+    if (selected) {
+      return [];
+    }
+    const mcpInfo = $mcpRegistriesServerInfos.find(mcp => mcp.id === id);
+
+    if (!mcpInfo) {
+      throw Error(`Suggested action ${suggestedAction.action} requires MCP with id ${id} but it was not found.`);
+    }
+
+    return [mcpInfo.name];
+  });
+
+  if (mcpsToSelect?.length) {
+    mcpSelectorOpen = true;
+
+    toast.error(`You need to select the following MCP first: ${mcpsToSelect.join(', ')}`);
+    return;
+  }
+
+  await chatClient.sendMessage({
+    role: 'user',
+    parts: [{ text: suggestedAction.action, type: 'text' }],
+  });
+}
 </script>
 
 <div class="grid w-full gap-2 sm:grid-cols-2">
@@ -51,38 +114,7 @@ const suggestedActions = [
 		>
 			<Button
 				variant="ghost"
-				onclick={async (): Promise<void> => {
-
-          const mcpsToEnable = suggestedAction.requiredMcp?.flatMap(id => {
-
-            const selected = selectedMCP.find(mcp => mcp.id === id);
-            const mcpInfo = $mcpRemoteServerInfos.find(mcp => mcp.id === id);
-
-           if (!mcpInfo)
-           {
-            throw Error(`Suggested action ${suggestedAction.action} requires MCP with id ${id} but it was not found.`);
-           }
-
-            if (selected)
-          {
-            return [];
-          } 
-          return [mcpInfo.name];
-          });
-
-
-					if (mcpsToEnable?.length) {
-              mcpSelectorOpen = true;
-
-    					toast.error(`You need to select the following MCP first: ${mcpsToEnable.join(', ')}`);
-						return;
-					}
-
-      await chatClient.sendMessage({
-						role: 'user',
-						parts: [{text: suggestedAction.action, type: 'text' }],
-					});
-				}}
+				onclick={onclick.bind(undefined, suggestedAction)}
 				class="h-auto w-full flex-1 items-start justify-start gap-1 rounded-xl border px-4 py-3.5 text-left text-sm sm:flex-col"
 			>
 				<span class="font-medium">{suggestedAction.title}</span>
