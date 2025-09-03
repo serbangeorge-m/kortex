f3250f696092f2d02af06b778520e43528d43de6
Florent Benoit <fbenoit@redhat.com>
2025-09-03 09:07:16 +0200

    chore: remove accounts
    fixes #247
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

--- packages/renderer/src/AppNavigation.svelte
+++ packages/renderer/src/AppNavigation.svelte
@@ -1,17 +1,14 @@
 <svelte:options runes={true} />
 
 <script lang="ts">
-import { Tooltip } from '@podman-desktop/ui-svelte';
 import { onDestroy, onMount } from 'svelte';
 import type { TinroRouteMeta } from 'tinro';
 
 import { NavigationPage } from '/@api/navigation-page';
 
 import { AppearanceSettings } from '../../main/src/plugin/appearance-settings';
-import AuthActions from './lib/authentication/AuthActions.svelte';
 import { CommandRegistry } from './lib/CommandRegistry';
 import NewContentOnDashboardBadge from './lib/dashboard/NewContentOnDashboardBadge.svelte';
-import AccountIcon from './lib/images/AccountIcon.svelte';
 import MessageIcon from './lib/images/MessageIcon.svelte';
 import SettingsIcon from './lib/images/SettingsIcon.svelte';
 import NavItem from './lib/ui/NavItem.svelte';
@@ -26,8 +23,6 @@ interface Props {
 }
 let { exitSettingsCallback, meta = $bindable() }: Props = $props();
 
-let authActions = $state<AuthActions>();
-let outsideWindow = $state<HTMLDivElement>();
 let iconWithTitle = $state(false);
 
 const iconSize = '22';
@@ -96,22 +91,6 @@ function onDidChangeConfigurationCallback(e: Event): void {
   {/each}
 
   <div class="grow"></div>
-
-  <div bind:this={outsideWindow}>
-    <NavItem href="/accounts" tooltip="" bind:meta={meta} onClick={(event): void => authActions?.onButtonClick(event)}>
-      <Tooltip bottomRight tip="Accounts">
-        <div class="flex flex-col items-center w-full h-full">
-          <AccountIcon size={iconSize} />
-          {#if iconWithTitle}
-            <div class="text-xs text-center ml-[2px]" aria-label="Accounts title">
-              Accounts
-            </div>
-          {/if}
-        </div>
-      </Tooltip>
-      <AuthActions bind:this={authActions} outsideWindow={outsideWindow} />
-    </NavItem>
-  </div>
 
   <NavItem href="/preferences" tooltip="Settings" bind:meta={meta} onClick={handleClick}>
     <SettingsIcon size={iconSize} />
