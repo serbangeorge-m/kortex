c36aa659c573c807fc268c817e24478798d6fc20
axel7083 <42176370+axel7083@users.noreply.github.com>
2025-09-03 08:58:44 +0200

    feat(FlowDetails): better Kubernetes page (#234)
    * feat(FlowDetails): auto generate Kubernetes YAML
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/renderer/src/lib/flows/FlowDetails.svelte
+++ packages/renderer/src/lib/flows/FlowDetails.svelte
@@ -1,14 +1,20 @@
 <script lang="ts">
-import { Button, Checkbox, Tab } from '@podman-desktop/ui-svelte';
+import { Button, Checkbox, Spinner, Tab } from '@podman-desktop/ui-svelte';
 import { onMount } from 'svelte';
 import { router } from 'tinro';
 
 import MonacoEditor from '/@/lib/editor/MonacoEditor.svelte';
+import KubernetesIcon from '/@/lib/kube/KubernetesIcon.svelte';
 import DetailsPage from '/@/lib/ui/DetailsPage.svelte';
+import KubernetesCurrentContextConnectionBadge from '/@/lib/ui/KubernetesCurrentContextConnectionBadge.svelte';
 import { getTabUrl, isTabSelected } from '/@/lib/ui/Util';
 import Route from '/@/Route.svelte';
 import { executeFlowsInfo } from '/@/stores/flows-execute';
+import { kubernetesContextsHealths } from '/@/stores/kubernetes-context-health';
+import { kubernetesContexts } from '/@/stores/kubernetes-contexts';
+import { kubernetesCurrentContextState } from '/@/stores/kubernetes-contexts-state';
 import { providerInfos } from '/@/stores/providers';
+import type { KubeContext } from '/@api/kubernetes-context';
 import type { ProviderFlowConnectionInfo } from '/@api/provider-info';
 
 import FlowActions from './FlowActions.svelte';
@@ -21,6 +27,15 @@ interface Props {
 }
 
 let { providerId, connectionName, flowId }: Props = $props();
+
+const currentContext: KubeContext | undefined = $derived($kubernetesContexts?.find(c => c.currentContext));
+const clusterReachable: boolean = $derived(
+  $kubernetesContextsHealths.find(({ contextName }) => contextName === currentContext?.name)?.reachable ??
+    $kubernetesCurrentContextState?.reachable,
+);
+
+let hideSecretsKubernetesYAML = $state(true);
+let loading: boolean = $state(false);
 
 let provider = $derived($providerInfos.find(provider => provider.id === providerId));
 let connection: ProviderFlowConnectionInfo | undefined = $derived(
@@ -36,8 +51,6 @@ let flowInfo = $derived({
 });
 
 let kubernetes: string | undefined = $state(undefined);
-
-let hideSecrets: boolean = $state(true);
 
 let flowContent: string | undefined = $state(undefined);
 
@@ -58,7 +71,7 @@ $effect(() => {
   }
 });
 
-async function deployKubernetes(dryrun: boolean): Promise<void> {
+async function refreshKubernetes(checked: boolean): Promise<void> {
   if (!provider) return;
   if (!connection) return;
 
@@ -69,24 +82,50 @@ async function deployKubernetes(dryrun: boolean): Promise<void> {
       connectionName: connection.name,
     },
     {
-      hideSecrets: dryrun ? hideSecrets : false, // overwrite hideSecrets when deploying to kubernetes
+      hideSecrets: checked,
       namespace: 'default',
-      dryrun: dryrun,
+      dryrun: true,
     },
   );
   kubernetes = result;
-  if (!dryrun) {
+}
+
+async function applyKubernetes(): Promise<void> {
+  if (!provider) return;
+  if (!connection) return;
+
+  try {
+    await window.flowDeployKubernetes(
+      {
+        flowId: flowId,
+        providerId: provider.id,
+        connectionName: connection.name,
+      },
+      {
+        hideSecrets: false,
+        namespace: 'default',
+        dryrun: false,
+      },
+    );
     router.goto('/jobs');
+  } catch (err: unknown) {
+    console.error('something went wrong while applying resources', err);
   }
 }
 
 onMount(() => {
-  window
-    .readFlow(providerId, connectionName, flowId)
-    .then(content => {
+  loading = true;
+
+  Promise.allSettled([
+    window.readFlow(providerId, connectionName, flowId).then(content => {
       flowContent = content;
-    })
-    .catch(console.error);
+    }),
+    refreshKubernetes(true),
+  ])
+    .catch(console.error)
+    .finally(() => {
+      loading = false;
+    });
 });
 
 function setSelectedFlowExecuteId(flowExecuteId: string): void {
@@ -94,11 +133,11 @@ function setSelectedFlowExecuteId(flowExecuteId: string): void {
 }
 </script>
 
-<DetailsPage title={path}>
+<DetailsPage  title={path}>
   {#snippet tabsSnippet()}
     <Tab title="Summary" selected={isTabSelected($router.path, 'summary')} url={getTabUrl($router.path, 'summary')} />
     <Tab title="Source" selected={isTabSelected($router.path, 'source')} url={getTabUrl($router.path, 'source')} />
-    <Tab title="Kube" selected={isTabSelected($router.path, 'kube')} url={getTabUrl($router.path, 'kube')} />
+    <Tab title="Kubernetes" selected={isTabSelected($router.path, 'kubernetes')} url={getTabUrl($router.path, 'kubernetes')} />
     {#if flowExecutions.length > 0 || isTabSelected($router.path, 'run') }
       <Tab title="Run ({flowExecutions.length})" selected={isTabSelected($router.path, 'run')} url={getTabUrl($router.path, 'run')} />
     {/if}
@@ -110,32 +149,43 @@ function setSelectedFlowExecuteId(flowExecuteId: string): void {
         onLocalRun={setSelectedFlowExecuteId} />
     {/snippet}
   {#snippet contentSnippet()}
-    <Route path="/summary" breadcrumb="Summary" navigationHint="tab">
-      <ul>
-        <li>{providerId} => {provider?.name}</li>
-        <li>{connectionName} => {connection?.name}</li>
-        <li>{flowId} => {path}</li>
-      </ul>
-    </Route>
-    <Route path="/source" breadcrumb="Source" navigationHint="tab">
-      <MonacoEditor content={flowContent} language="yaml" readOnly={true} />
-    </Route>
-    <Route path="/kube" breadcrumb="Kube" navigationHint="tab">
-      <div class="flex flex-row gap-x-2 items-center">
-        <Checkbox bind:checked={hideSecrets} title="Hide Secrets">Hide Secret</Checkbox>
-
-        <Button onclick={deployKubernetes.bind(undefined, true)}>Dryrun</Button>
-        <Button onclick={deployKubernetes.bind(undefined, false)}>Apply</Button>
+    {#if loading}
+      <div>
+        <Spinner/>
       </div>
+    {:else}
+      <Route path="/summary" breadcrumb="Summary" navigationHint="tab">
+        <ul>
+          <li>{providerId} => {provider?.name}</li>
+          <li>{connectionName} => {connection?.name}</li>
+          <li>{flowId} => {path}</li>
+        </ul>
+      </Route>
+      <Route path="/source" breadcrumb="Source" navigationHint="tab">
+        <MonacoEditor content={flowContent} language="yaml" readOnly={true} />
+      </Route>
+      <Route path="/kubernetes" breadcrumb="Kube" navigationHint="tab">
+        <div class="flex flex-row px-4 items-center justify-between">
+          <Checkbox bind:checked={hideSecretsKubernetesYAML} onclick={refreshKubernetes} title="Hide Secrets">Hide Secret</Checkbox>
+          <div class="flex flex-row gap-x-2">
+            <KubernetesCurrentContextConnectionBadge />
+            <Button
+              disabled={!clusterReachable}
+              icon={KubernetesIcon}
+              onclick={applyKubernetes}>
+              Apply
+            </Button>
+          </div>
 
-      {#if kubernetes}
-        <MonacoEditor content={kubernetes} language="yaml" readOnly={true} />
-      {/if}
-    </Route>
-    <Route path="/run" breadcrumb="Run ({flowExecutions.length})" navigationHint="tab">
-      <FlowDetailsRun {providerId} {connectionName} {flowId} {flowExecutions} bind:selectedFlowExecuteId={selectedFlowExecuteId}/>
-    </Route>
-
+        </div>
+        {#if kubernetes}
+          <MonacoEditor content={kubernetes} language="yaml" readOnly={true} />
+        {/if}
+      </Route>
+      <Route path="/run" breadcrumb="Run ({flowExecutions.length})" navigationHint="tab">
+        <FlowDetailsRun {providerId} {connectionName} {flowId} {flowExecutions} selectedFlowExecuteId={selectedFlowExecuteId}/>
+      </Route>
+    {/if}
   {/snippet}
 </DetailsPage>
 
