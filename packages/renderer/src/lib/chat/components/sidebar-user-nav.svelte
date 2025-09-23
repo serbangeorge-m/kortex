1d084e46129d5a5c1ad016be49c7707f3f3c225a
Simon Rey <51708585+simonrey1@users.noreply.github.com>
2025-09-23 14:41:33 +0200

    chore(chat): remove unused buttons (#427)
    Signed-off-by: Simon Rey <sfbrey+eqqe@gmail.com>

--- packages/renderer/src/lib/chat/components/sidebar-user-nav.svelte
+++ packages/renderer/src/lib/chat/components/sidebar-user-nav.svelte
@@ -1,65 +1,12 @@
 <script lang="ts">
-import { getTheme } from '@sejohnson/svelte-themes';
-
-import { cn } from '/@//lib/chat/utils/shadcn';
-
 import type { User } from '../../../../../main/src/chat/db/schema';
-import ChevronUp from './icons/chevron-up.svelte';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from './ui/dropdown-menu';
-import { SidebarMenu, SidebarMenuButton, SidebarMenuItem } from './ui/sidebar';
+import { SidebarMenu, SidebarMenuItem } from './ui/sidebar';
 
 let { user }: { user: User } = $props();
-const theme = getTheme();
 </script>
 
 <SidebarMenu>
 	<SidebarMenuItem>
-		<DropdownMenu>
-			<DropdownMenuTrigger>
-				{#snippet child({ props })}
-					<SidebarMenuButton
-						{...props}
-						class="bg-background data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground h-10"
-					>
-						<img
-							src={`https://avatar.vercel.sh/${user.email}`}
-							alt={user.email ?? 'User Avatar'}
-							width={24}
-							height={24}
-							class="rounded-full"
-						/>
-						<span class="truncate">{user?.email}</span>
-						<ChevronUp class="ml-auto" />
-					</SidebarMenuButton>
-				{/snippet}
-			</DropdownMenuTrigger>
-			<DropdownMenuContent side="top" class="w-[--bits-floating-anchor-width]">
-				<DropdownMenuItem
-					class="cursor-pointer"
-					onSelect={(): string =>
-						(theme.selectedTheme = theme.resolvedTheme === 'light' ? 'dark' : 'light')}
-				>
-					Toggle {theme.resolvedTheme === 'light' ? 'dark' : 'light'} mode
-				</DropdownMenuItem>
-				<DropdownMenuSeparator />
-				<DropdownMenuItem>
-					{#snippet child({ props })}
-						<a
-							{...props}
-							href="/signout"
-							class={cn('w-full cursor-pointer', props.class as string)}
-							data-sveltekit-preload-data="false"
-							data-sveltekit-reload>Sign out</a
-						>
-					{/snippet}
-				</DropdownMenuItem>
-			</DropdownMenuContent>
-		</DropdownMenu>
+		<span class="truncate">{user?.email}</span>
 	</SidebarMenuItem>
 </SidebarMenu>
