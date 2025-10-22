4f483f98b32b7b755fbab9893c4df600e78c5ea1
Florent Benoit <fbenoit@redhat.com>
2025-10-22 10:53:50 +0200

    feat: register the command line generation for flow connections
    related to scheduling
    
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

--- packages/extension-api/src/extension-api.d.ts
+++ packages/extension-api/src/extension-api.d.ts
@@ -418,6 +418,10 @@ declare module '@kortex-app/api' {
     namespace: string;
   }
 
+  export interface FlowGenerateCommandLineOptions {
+    flowId: string;
+  }
+
   export interface FlowGenerateCommandLineResult {
     command: string;
     args: string[];
@@ -471,6 +475,12 @@ declare module '@kortex-app/api' {
        * @experimental expect change
        */
       generate(options?: FlowGenerateOptions): Promise<string>;
+
+      /**
+       * @experimental expect change
+       */
+      generateCommandLine(options: FlowGenerateCommandLineOptions): Promise<FlowGenerateCommandLineResult>;
+
       /**
        * @experimental expect change
        */
